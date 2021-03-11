852001760d3cbb9f90509c9bb109c723d388beb7
unintendedbear <unintendedbear@gmail.com>
2021-03-11 20:14:50 +0100

    Traducción cuenta atrás

--- js/script.js
+++ js/script.js
@@ -368,7 +368,7 @@
 		$('.time-countdown').each(function() {
 		var $this = $(this), finalDate = $(this).data('countdown');
 		$this.countdown(finalDate, function(event) {
-			var $this = $(this).html(event.strftime('' + '<div class="counter-column"><span class="count">%D</span>Days</div> ' + '<div class="counter-column"><span class="count">%H</span>Hours</div>  ' + '<div class="counter-column"><span class="count">%M</span>Minutes</div>  ' + '<div class="counter-column"><span class="count">%S</span>Seconds</div>'));
+			var $this = $(this).html(event.strftime('' + '<div class="counter-column"><span class="count">%D</span>Días</div> ' + '<div class="counter-column"><span class="count">%H</span>Horas</div>  ' + '<div class="counter-column"><span class="count">%M</span>Minutos</div>  ' + '<div class="counter-column"><span class="count">%S</span>Segundos</div>'));
 		});
 	 });
 	}
